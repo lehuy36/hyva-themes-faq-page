93d0c2732d233b099b6f9042e07e465050efbf16
lehuy36 <leconghuy.dev@gmail.com>
2025-05-08 16:06:35 +0700

    feat: add basic FAQ HTML layout with Alpine.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
